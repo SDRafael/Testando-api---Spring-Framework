47bb52c500b13812badf896dc6a27d56695a266e
sdrafael <j.rafael_jr@outlook.com>
2024-11-15 17:14:19 -0300

    refinando a exibição das buscas e utilizando lambdas

--- src/main/java/br/com/alura/screenmatch/interact/Menu.java
+++ src/main/java/br/com/alura/screenmatch/interact/Menu.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch.interact;
 
+import br.com.alura.screenmatch.models.DataEpisode;
 import br.com.alura.screenmatch.models.DataSeason;
 import br.com.alura.screenmatch.models.DataSerie;
 import br.com.alura.screenmatch.service.ConsumoAPI;
@@ -16,7 +17,7 @@ public class Menu {
     private Scanner busca = new Scanner(System.in);
     private ConsumoAPI consumoApi = new ConsumoAPI();
     private ConverteDados conversor = new ConverteDados();
-    public void exibeMenu(){
+    public void exibeMenu() {
         System.out.println("Digite o nome da série: ");
 
         String serieNome = busca.nextLine();
@@ -24,12 +25,23 @@ public class Menu {
         DataSerie dados = conversor.obterDados(json, DataSerie.class);
         System.out.println(dados);
         List<DataSeason> temporadas = new ArrayList<>();
-        for (int i = 1; i <= dados.totalTemporadas(); i++){
+        for (int i = 1; i <= dados.totalTemporadas(); i++) {
             json = consumoApi.obterDados(MAIN_URL+serieNome.replace(" ", "+")+"&season="+i+API_KEY);
             DataSeason dadosTem = conversor.obterDados(json, DataSeason.class);
             temporadas.add(dadosTem);
 
         }
-        temporadas.forEach(System.out::println);
+//        temporadas.forEach(System.out::println);
+//        List<DataSeason> epNameBySeason = new ArrayList<>();
+//        for (int i = 0; i < dados.totalTemporadas(); i++) {
+//            List<DataEpisode> seasonEpisode = temporadas.get(i).episodios();
+//            for (int j = 0; j < seasonEpisode.size(); j++) {
+//                System.out.println(seasonEpisode.get(j).nome());
+//
+//            }
+//
+//        }
+        // IMPLEMENTANDO UMA BUSCA POR NOME COM ARTIFÍCIO DAS LAMBDAS
+        temporadas.forEach(temp -> temp.episodios().forEach(ep -> System.out.println(ep.nome())));
     }
 }
